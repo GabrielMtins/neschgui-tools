5684580bd7ff0c0144cf452706a8043ba4620511
Gabriel Martins <gabrielmartinsthe12381@gmail.com>
2026-05-29 03:44:30 -0300

    first commit

---
+++ rom_tools/main.c
@@ -0,0 +1,7 @@
+#include "rom_tools.h"
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World!\n");
+}
---
+++ rom_tools/rom_tools.h
@@ -0,0 +1,124 @@
+#ifndef ROM_TOOLS_H
+#define ROM_TOOLS_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define ROM_TOOLS_IMPLEMENTATION
+
+#define FOR_TILE_TYPE_LIST(DO) \
+	DO(ROM_TYPE_NES, Rom_NesTile, Nes_GetColor, Nes_SetColor) \
+	DO(ROM_TYPE_GB, Rom_GbTile, Gb_GetColor, Gb_SetColor)
+
+typedef enum {
+	#define EXPAND_AS_ENUM(id, type, get, set) id,
+	FOR_TILE_TYPE_LIST(EXPAND_AS_ENUM)
+	#undef EXPAND_AS_ENUM
+} Rom_Format;
+
+typedef struct {
+	size_t num_tiles;
+	Rom_Format format;
+
+	void *data;
+} Rom_Viewer;
+
+Rom_Viewer Rom_CreateViewer(Rom_Format format, void *data, size_t size);
+
+uint8_t Rom_GetTilePixelColor(Rom_Viewer *viewer, size_t tile_id, uint8_t x, uint8_t y);
+
+void Rom_SetTilePixelColor(Rom_Viewer *viewer, size_t tile_id, uint8_t x, uint8_t y, uint8_t c);
+
+#ifdef ROM_TOOLS_IMPLEMENTATION
+
+#define SET_BIT(byte, bit, value) ((byte) = ((byte) & ~(1 << (bit))) | (((value) & 1) << (bit)))
+#define GET_BIT(byte, bit) (((byte) >> (bit)) & 1)
+
+typedef struct {
+	uint8_t plane0[8];
+	uint8_t plane1[8];
+} Rom_NesTile;
+
+typedef struct {
+	uint8_t plane0[8];
+	uint8_t plane1[8];
+} Rom_GbTile;
+
+static uint8_t Nes_GetColor(Rom_NesTile *tile, uint8_t x, uint8_t y) {
+	x = 7 - x;
+
+	return
+		(GET_BIT(tile->plane1[y], x) << 1) |
+		(GET_BIT(tile->plane0[y], x));
+}
+
+static void Nes_SetColor(Rom_NesTile *tile, uint8_t x, uint8_t y, uint8_t c) {
+	x = 7 - x;
+
+	SET_BIT(tile->plane0[y], x, c);
+	SET_BIT(tile->plane1[y], x, c >> 1);
+}
+
+static uint8_t Gb_GetColor(Rom_GbTile *tile, uint8_t x, uint8_t y) {
+	return
+		(GET_BIT(tile->plane1[y], x) << 1) |
+		(GET_BIT(tile->plane0[y], x));
+}
+
+static void Gb_SetColor(Rom_GbTile *tile, uint8_t x, uint8_t y, uint8_t c) {
+	SET_BIT(tile->plane0[y], x, c);
+	SET_BIT(tile->plane1[y], x, c >> 1);
+}
+
+Rom_Viewer Rom_CreateViewer(Rom_Format format, void *data, size_t size) {
+	Rom_Viewer viewer;
+
+	viewer.data = data;
+	viewer.format = format;
+
+	#define EXPAND_AS_CASE(id, type, get, set) case id: viewer.num_tiles = size / sizeof(type); break;
+
+	switch(format) {
+		FOR_TILE_TYPE_LIST(EXPAND_AS_CASE)
+	}
+
+	#undef EXPAND_AS_CASE
+
+	return viewer;
+}
+
+uint8_t Rom_GetTilePixelColor(Rom_Viewer *viewer, size_t tile_id, uint8_t x, uint8_t y) {
+	uint8_t ret_value = 0xff;
+
+	if(tile_id >= viewer->num_tiles) {
+		return 0xff;
+	}
+
+	#define EXPAND_AS_CASE(id, type, get, set) case id: ret_value = get(((type *) viewer->data) + tile_id, x, y); break;
+
+	switch(viewer->format) {
+		FOR_TILE_TYPE_LIST(EXPAND_AS_CASE);
+	}
+
+	#undef EXPAND_AS_CASE
+
+	return ret_value;
+}
+
+void Rom_SetTilePixelColor(Rom_Viewer *viewer, size_t tile_id, uint8_t x, uint8_t y, uint8_t c) {
+	if(tile_id >= viewer->num_tiles) {
+		return;
+	}
+
+	#define EXPAND_AS_CASE(id, type, get, set) case id: set(((type *) viewer->data) + tile_id, x, y, c); break;
+
+	switch(viewer->format) {
+		FOR_TILE_TYPE_LIST(EXPAND_AS_CASE)
+	}
+
+	#undef EXPAND_AS_CASE
+}
+
+#endif
+
+#endif
