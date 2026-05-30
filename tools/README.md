292fd6961a8ba42220c7f62428f66936d0fc940c
Gabriel Martins <gabrielmartinsthe12381@gmail.com>
2026-05-30 19:46:52 -0300

    changed files organization

--- tools/README.md
+++ tools/README.md
@@ -11,7 +11,7 @@ $ make
 ## nes2png
 
 **nes2png** can convert ROM files to PNG files with a given palette color.
-Currently, it supports only NES and Game Boy ROMs. Usage:
+Usage:
 
 ```
 nes2png < rom.nes > out.png
@@ -35,3 +35,32 @@ The palette file should contain one hexadecimal 24bit color per line:
 ```
 
 If no palette is specified, a default grayscale palette is used.
+
+## png2nes
+
+**png2nes** can convert PNG files to ROM files with a given palette color.
+Usage:
+
+```
+png2nes < out.png > rom.nes
+```
+
+Options:
+- `--format-nes` - NES format (default).
+- `--format-gb` - Game Boy format.
+- `--num-tiles-total` - Set the total number of tiles to be read by the
+  software from the given png.
+- `--palette "#ffffff #c0c0c0 #808080 #000000"` - set palette colors.
+- `--palette-file colors.txt` - read palette from a file.
+
+## nescount
+
+**nescount** counts the number of tiles of a given file. Usage:
+
+```
+nescount < rom.nes
+```
+
+Options:
+- `--format-nes` - NES format (default).
+- `--format-gb` - Game Boy format.
---
+++ tools/src/png2nes.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rom_tools.h"
+#include "util.h"
+
+#include <math.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int get_color_id(uint32_t color) {
+	for(int i = 0; i < PALETTE_SIZE; i++) {
+		if(color == palette[i]) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+void usage(const char *prog) {
+	fprintf(
+			stderr,
+			"Usage: %s [OPTIONS]",
+			prog
+		   );
+}
+
+int main(int argc, char **argv) {
+	uint32_t *raw_img;
+	uint32_t *img;
+	void *data;
+	size_t data_size;
+	size_t img_size;
+	int width, height;
+	int num_tiles_total = 0;
+	int num_tiles_row = 16;
+	Rom_Format format = ROM_TYPE_NES;
+	Rom_Viewer viewer;
+
+	for(int i = 1; i < argc; i++) {
+		if(!strcmp(argv[i], "--format-nes")) {
+			format = ROM_TYPE_NES;
+		} else if(!strcmp(argv[i], "--format-gb")) {
+			format = ROM_TYPE_GB;
+		} else if(i == argc - 1) {
+			continue;
+		} else if(!strcmp(argv[i], "--palette")) {
+			read_palette(argv[i + 1]);
+			i++;
+		} else if(!strcmp(argv[i], "--palette-file")) {
+			if(read_palette_file(argv[i + 1]) != 0) {
+				fprintf(stderr, "%s: Failed to read palette file: %s\n", argv[0], argv[i + 1]);
+				return -1;
+			}
+
+			i++;
+		} else if(!strcmp(argv[i], "--num-tiles-total")) {
+			if(sscanf(argv[i + 1], "%d", &num_tiles_total) != 1) {
+				fprintf(stderr, "%s: Failed to num-tiles-total", argv[0]);
+				return -1;
+			}
+
+			i++;
+		} else {
+			usage(argv[0]);
+			return -1;
+		}
+	}
+
+	raw_img = stdin_read(&img_size);
+
+	img = (uint32_t *) stbi_load_from_memory(
+			(const unsigned char *) raw_img, 
+			img_size,
+			&width,
+			&height,
+			NULL,
+			4
+			);
+
+	num_tiles_row = width / TILE_SIZE;
+
+	if(num_tiles_total == 0) {
+		num_tiles_total = num_tiles_row * (height / TILE_SIZE);
+	}
+
+	data_size = Rom_GetRomSizeByTiles(format, num_tiles_total);
+
+	data = malloc(data_size);
+
+	viewer = Rom_CreateViewer(format, data, data_size);
+
+	for(int j = 0; j < height; j++) {
+		for(int i = 0; i < width; i++) {
+			int tile_id = (i / TILE_SIZE) + (j / TILE_SIZE) * num_tiles_row;
+
+			int color = get_color_id(img[i + j * width]);
+
+			if(color < -1) {
+				fprintf(stderr, "%s: Image contains a color not in the palette.", argv[0]);
+				return -1;
+			}
+
+			Rom_SetTilePixelColor(
+					&viewer,
+					tile_id,
+					i % TILE_SIZE,
+					j % TILE_SIZE,
+					color
+					);
+		}
+	}
+
+	fwrite(data, 1, data_size, stdout);
+
+	free(data);
+	free(raw_img);
+	stbi_image_free(img);
+}
